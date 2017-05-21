d792e243be12dcb3fe237e2dd88a3d9b70df8c8d
David Beckwith <thidreplicator@gmail.com>
2017-05-21 06:03:04 +0000

    delete only keys whose buckets match /^test/

--- src/Raku.js
+++ src/Raku.js
@@ -9,25 +9,25 @@ const DEFAULT_SETS_BUCKET_TYPE = 'sets'
 const KNOWN_BUCKET_TYPES = ['default', 'sets', 'counters']
 
 const sort_helper = (a, b) => {
-		let t_a = typeof a, t_b = typeof b
-		return t_a < t_b ? -1 : t_a > t_b ? 1 : a < b ? -1 : a > b ? 1 : 0
-	}
+    let t_a = typeof a, t_b = typeof b
+    return t_a < t_b ? -1 : t_a > t_b ? 1 : a < b ? -1 : a > b ? 1 : 0
+  }
 
 class Raku {
 
-	constructor() {
-		this.client = new NoRiak.Client()
-		this.NoRiak = NoRiak
-		// set(k, v) is an alias for put(k, v).
-		this.set = this.put
-		this.bucket = DEFAULT_BUCKET
+  constructor() {
+    this.client = new NoRiak.Client()
+    this.NoRiak = NoRiak
+    // set(k, v) is an alias for put(k, v).
+    this.set = this.put
+    this.bucket = DEFAULT_BUCKET
 
-		this.counter_bucket = DEFAULT_COUNTER_BUCKET
-		this.counter_bucket_type = DEFAULT_COUNTER_BUCKET_TYPE
+    this.counter_bucket = DEFAULT_COUNTER_BUCKET
+    this.counter_bucket_type = DEFAULT_COUNTER_BUCKET_TYPE
 
-		this.sets_bucket = DEFAULT_SETS_BUCKET
-		this.sets_bucket_type = DEFAULT_SETS_BUCKET_TYPE
-	}
+    this.sets_bucket = DEFAULT_SETS_BUCKET
+    this.sets_bucket_type = DEFAULT_SETS_BUCKET_TYPE
+  }
 
   getBucket(type) {
     let bucket, prepend = ''
@@ -48,179 +48,179 @@ class Raku {
     return prepend + bucket
   }
 
-	//----+
-	// KV |
-	//----+
-	put(k, v) {
-		return this.client.put({
-			bucket: this.getBucket(),
-			key: k,
-			content: { value: JSON.stringify(v) }
-		})
-	} // put
+  //----+
+  // KV |
+  //----+
+  put(k, v) {
+    return this.client.put({
+      bucket: this.getBucket(),
+      key: k,
+      content: { value: JSON.stringify(v) }
+    })
+  } // put
 
 
-	get(k) {
-		return this.client.get({
-				bucket: this.getBucket(),
-				key: k
-			})
-			.then(result => {
-				return result && JSON.parse(result.content[0].value.toString())
-			})
-	} // get
+  get(k) {
+    return this.client.get({
+        bucket: this.getBucket(),
+        key: k
+      })
+      .then(result => {
+        return result && JSON.parse(result.content[0].value.toString())
+      })
+  } // get
 
-	del(k, bucket, type) {
-		this.constructor.check_key(k)
+  del(k, bucket, type) {
+    this.constructor.check_key(k)
     let args = {key: k}
     args.bucket = bucket || this.getBucket()
     if (type) { args.type = type }
-		return this.client.del(args)
-	}
+    return this.client.del(args)
+  }
 
-	bget(bucket, k) {
-		return this.client.get({
-				bucket: bucket,
-				key: k
-			})
-			.then(result => {
-				return result && JSON.parse(result.content[0].value.toString())
-			})
-	} // get2
+  bget(bucket, k) {
+    return this.client.get({
+        bucket: bucket,
+        key: k
+      })
+      .then(result => {
+        return result && JSON.parse(result.content[0].value.toString())
+      })
+  } // get2
 
-	bset(bucket, k, v) {
-		return this.client.put({
-			bucket: bucket,
-			key: k,
-			content: { value: JSON.stringify(v) }
-		})
-	} // set3
+  bset(bucket, k, v) {
+    return this.client.put({
+      bucket: bucket,
+      key: k,
+      content: { value: JSON.stringify(v) }
+    })
+  } // set3
 
-	bdel(bucket, k) {
-		return this.del(k, bucket)
-	}
+  bdel(bucket, k) {
+    return this.del(k, bucket)
+  }
 
-	//----------+
-	// SETS |
-	//----------+
+  //----------+
+  // SETS |
+  //----------+
 
-	enbuf(v) {
-		const json = JSON.stringify(v) 
-		return Buffer.from(json, 'utf8')
-	}
+  enbuf(v) {
+    const json = JSON.stringify(v)
+    return Buffer.from(json, 'utf8')
+  }
 
-	debuf(buf) {
-		const v = buf.toString('utf8')
-		return JSON.parse(v)
-	} 
+  debuf(buf) {
+    const v = buf.toString('utf8')
+    return JSON.parse(v)
+  }
 
-	get_sets(k) {
-		this.constructor.check_key(k)
-		return new NoRiak.CRDT.Set(this.client,
-				{ bucket: this.getBucket('sets'),
-					type: this.sets_bucket_type,
-					key: k})
-	}
+  get_sets(k) {
+    this.constructor.check_key(k)
+    return new NoRiak.CRDT.Set(this.client,
+        { bucket: this.getBucket('sets'),
+          type: this.sets_bucket_type,
+          key: k})
+  }
 
-	sismember(k, v) {
-		return this.smembers(k)
-			.then(values => values.includes(v))
-	}
+  sismember(k, v) {
+    return this.smembers(k)
+      .then(values => values.includes(v))
+  }
 
-	sadd(k, ...values) {
-		let s = this.get_sets(k)
-		values.forEach(v => {
-			s.add(this.enbuf(v))
-		})
-		return s.save()
-	}
+  sadd(k, ...values) {
+    let s = this.get_sets(k)
+    values.forEach(v => {
+      s.add(this.enbuf(v))
+    })
+    return s.save()
+  }
 
   sreset(k, ...values) {
     return this.sdel(k)
       .then(_ => this.sadd(k, ...values))
   }
 
-	srem(k, v) {
-		let s = this.get_sets(k)
-		return s.load()
-			.then(res => {
+  srem(k, v) {
+    let s = this.get_sets(k)
+    return s.load()
+      .then(res => {
         return res.remove(this.enbuf(v)).save()
       })
       .catch(reason => {
-				if (reason.toString().match(/RiakError.*precondition.*not_present/)) {
+        if (reason.toString().match(/RiakError.*precondition.*not_present/)) {
           return -1
         }
       })
-	}
+  }
 
-	sdel(k) {
+  sdel(k) {
     return this.del(k, this.getBucket('sets'), this.sets_bucket_type)
-	}
+  }
 
-	smembers(k) {
-		this.constructor.check_key(k)
-		let s = this.get_sets(k)
-		return s.load()
-			.then(res => {
-				return res.value()
-									.map(v => this.debuf(v))
-									.sort(sort_helper)
-			 })
-	}
+  smembers(k) {
+    this.constructor.check_key(k)
+    let s = this.get_sets(k)
+    return s.load()
+      .then(res => {
+        return res.value()
+                  .map(v => this.debuf(v))
+                  .sort(sort_helper)
+       })
+  }
 
-	scard(k) {
-		return this.smembers(k)
-			.then(values => values.length)
-	}
+  scard(k) {
+    return this.smembers(k)
+      .then(values => values.length)
+  }
 
-	//----------+
-	// COUNTERS |
-	//----------+
+  //----------+
+  // COUNTERS |
+  //----------+
 
-	get_counter(k) {
-		this.constructor.check_key(k)
-		return new NoRiak.CRDT.Counter(this.client,
-				{ bucket: this.getBucket('counter'),
-					type: this.counter_bucket_type,
-					key: k})
-	}
+  get_counter(k) {
+    this.constructor.check_key(k)
+    return new NoRiak.CRDT.Counter(this.client,
+        { bucket: this.getBucket('counter'),
+          type: this.counter_bucket_type,
+          key: k})
+  }
 
-	cget(k) {
-		let counter = this.get_counter(k)
-		return counter.load()
-			.then(counter => counter.value().toNumber())
-	}
+  cget(k) {
+    let counter = this.get_counter(k)
+    return counter.load()
+      .then(counter => counter.value().toNumber())
+  }
 
-	cset(k, v) {
-		return this.cget(k)
-			.then(x => {
-				return this.get_counter(k).increment(-x + v).save()
-			})
-			.then(counter => counter.value().toNumber())
-	}
+  cset(k, v) {
+    return this.cget(k)
+      .then(x => {
+        return this.get_counter(k).increment(-x + v).save()
+      })
+      .then(counter => counter.value().toNumber())
+  }
 
-	cinc(k, v) {
-		let counter = this.get_counter(k)
-		let amount = (v == undefined) ? 1 : v
-		return counter.increment(amount).save()
-			.then(counter => counter.value().toNumber())
-	}
+  cinc(k, v) {
+    let counter = this.get_counter(k)
+    let amount = (v == undefined) ? 1 : v
+    return counter.increment(amount).save()
+      .then(counter => counter.value().toNumber())
+  }
 
-	cdec(k, v) {
-		let amount = (v == undefined) ? -1 : -v
-		return this.cinc(k, amount)
-	}
+  cdec(k, v) {
+    let amount = (v == undefined) ? -1 : -v
+    return this.cinc(k, amount)
+  }
 
-	cdel(k) {
-		return this.del(k, this.getBucket('counter'), this.counter_bucket_type)
-	}
+  cdel(k) {
+    return this.del(k, this.getBucket('counter'), this.counter_bucket_type)
+  }
 
-	// Make sure the key is a string.
-	static check_key(k) {
-		if (typeof k != 'string') {
-			throw new Error('The counter key must be a string.')
-		}
-	}
+  // Make sure the key is a string.
+  static check_key(k) {
+    if (typeof k != 'string') {
+      throw new Error('The counter key must be a string.')
+    }
+  }
 
   // This is not a real bucketType API, because Riak offers only a command-line interface to managing
   //  bucket types, which requires sudo.  You should manually list the known active bucketTypes by
@@ -237,7 +237,7 @@ class Raku {
 
   buckets(bucketType) {
     const bucketTypes = bucketType == undefined ? this.bucketTypes() : [ bucketType ]
-		return Promise.all(bucketTypes.map(type => this.client.listBuckets({type})))
+    return Promise.all(bucketTypes.map(type => this.client.listBuckets({type})))
       .then(buckets => buckets.reduce((accum, arr, i) => {
          const typedBuckets = arr.map(bucket => ({bucket, type: bucketTypes[i]}))
          return accum.concat(typedBuckets)
@@ -252,9 +252,9 @@ class Raku {
       get_buckets = this.buckets()
     }
     return get_buckets
-			.then(bucket_list => {
+      .then(bucket_list => {
         __buckets = bucket_list
-				return Promise.all(bucket_list.map(args => this.client.listKeys(args)))
+        return Promise.all(bucket_list.map(args => this.client.listKeys(args)))
       })
       .then(keynames_by_bucket => {
         const keys = []
@@ -270,8 +270,10 @@ class Raku {
     if (!force && process.env.NODE_ENV != 'test') {
       throw 'Error Raku.deleteAll(): refused to delete all keys: must call deleteAll(true) or be in test environment.'
     }
-    return this.keys() 
-      .then(keyData => {
+    return this.keys()
+      .then(key_data => {
+        // Delete only test data.
+        const test_keys = key_data.filter(k => (typeof k.bucket == 'string') && (k.bucket.match(/^test/)))
         return Promise.all(keyData.map(args => this.client.del(args)))
       })
   }
@@ -293,6 +295,6 @@ Raku.DEFAULT_COUNTER_BUCKET = DEFAULT_COUNTER_BUCKET
 Raku.DEFAULT_COUNTER_BUCKET_TYPE = DEFAULT_COUNTER_BUCKET_TYPE
 Raku.DEFAULT_SETS_BUCKET = DEFAULT_SETS_BUCKET
 Raku.DEFAULT_SETS_BUCKET_TYPE = DEFAULT_SETS_BUCKET_TYPE
-Raku.KNOWN_BUCKET_TYPES  = KNOWN_BUCKET_TYPES 
+Raku.KNOWN_BUCKET_TYPES  = KNOWN_BUCKET_TYPES
 
 export default Raku
